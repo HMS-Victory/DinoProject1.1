dea7c971098e439f5b762bfc2b4fbdbfd677e928
HMS-Victory <banrabas.sacket@gmail.com>
2023-11-07 11:26:51 -0700

    1.0.2

--- scenes/Athlete.gd
+++ scenes/Athlete.gd
@@ -18,78 +18,75 @@ extends CharacterBody2D
 
 
 func _physics_process(delta):
-	$".".visible=false
-	if(Global.character=='Athlete'):
-		visible=true
-		move_and_slide()
-		velocity.y += Global.gravity 
+	move_and_slide()
+	velocity.y += Global.gravity 
 
-		if !Global.gameover:
-			#ifs for player physics
-			if Input.is_action_just_pressed("Speed"): #if statment for movement
-				Global.dificultySpeed += Global.speedUp
-			elif Input.is_action_just_released("Speed"):
-				Global.dificultySpeed -= Global.speedUp
-			elif Input.is_action_just_pressed("Slow"):
-				Global.dificultySpeed  += Global.slowDown
-			elif Input.is_action_just_released("Slow"):
-				Global.dificultySpeed -= Global.slowDown
-			elif Input.is_action_just_pressed("Jump") && is_on_floor():
-				velocity.y += Global.groundJump
-			elif Input.is_action_just_pressed("Jump") && !is_on_floor():
-				velocity.y += Global.airJump
-			elif Input.is_action_just_pressed("Throw") && !is_on_floor():
-				velocity.y += Global.groundJump
-				#allows you to keep jumping infinitely
-				if !Global.cheatsEnabled:
-					Global.coins -= 1
-				#print("coins %s" % Global.coins)
-			elif Input.is_action_just_pressed("cheats"):
-				Global.cheatsEnabled=true
-			else:
-				Global.dificultySpeed = Global.dificultySpeed
-			pass
+	if !Global.gameover:
+		#ifs for player physics
+		if Input.is_action_just_pressed("Speed"): #if statment for movement
+			Global.dificultySpeed += Global.speedUp
+		elif Input.is_action_just_released("Speed"):
+			Global.dificultySpeed -= Global.speedUp
+		elif Input.is_action_just_pressed("Slow"):
+			Global.dificultySpeed  += Global.slowDown
+		elif Input.is_action_just_released("Slow"):
+			Global.dificultySpeed -= Global.slowDown
+		elif Input.is_action_just_pressed("Jump") && is_on_floor():
+			velocity.y += Global.groundJump
+		elif Input.is_action_just_pressed("Jump") && !is_on_floor():
+			velocity.y += Global.airJump
+		elif Input.is_action_just_pressed("Throw") && !is_on_floor():
+			velocity.y += Global.groundJump
+			#allows you to keep jumping infinitely
+			if !Global.cheatsEnabled:
+				Global.coins -= 1
+			#print("coins %s" % Global.coins)
+		elif Input.is_action_just_pressed("cheats"):
+			Global.cheatsEnabled=true
+		else:
+			Global.dificultySpeed = Global.dificultySpeed
+		pass
 
-			#ifs for animations
-			if  Input.is_action_just_pressed("Jump") && is_on_floor():
-				ap.play("Jumping")
-			elif Input.is_action_just_pressed("Jump") && !is_on_floor():
-				ap.play("Jumping")
-			elif Input.is_action_just_pressed("Throw") && !is_on_floor():
-				ap.play("Jumping")
-			elif is_on_floor(): 
-				ap.play("Running")
-			if Input.is_action_pressed("Speed"):
-				sprite.rotation_degrees = 7.5
-			elif Input.is_action_just_released("Speed"):
-				sprite.rotation_degrees = 0
-			elif Input.is_action_pressed("Slow"):
-				sprite.rotation_degrees = -7.5
-			elif Input.is_action_just_released("Slow"):
-				sprite.rotation_degrees = 0
-			
-			#ifs for particles
-			if Input.is_action_just_pressed("Throw") && !is_on_floor():
-				spendToJump.emitting = true
-			elif Input.is_action_pressed("Slow") && is_on_floor():
-				skidding.emitting = true
-			elif Input.is_action_pressed("Speed") && is_on_floor():
-				run.emitting = true
-			elif Input.is_action_pressed("Jump") && is_on_floor():
-				jump.emitting = true
-			elif Input.is_action_pressed("Jump") && Input.is_action_pressed("Speed"):
-				jump.emitting = true
-			elif Input.is_action_pressed("Jump") &&  Input.is_action_pressed("Slow"):
-				jump.emitting = true
+		#ifs for animations
+		if  Input.is_action_just_pressed("Jump") && is_on_floor():
+			ap.play("Jumping")
+		elif Input.is_action_just_pressed("Jump") && !is_on_floor():
+			ap.play("Jumping")
+		elif Input.is_action_just_pressed("Throw") && !is_on_floor():
+			ap.play("Jumping")
+		elif is_on_floor(): 
+			ap.play("Running")
+		if Input.is_action_pressed("Speed"):
+			sprite.rotation_degrees = 7.5
+		elif Input.is_action_just_released("Speed"):
+			sprite.rotation_degrees = 0
+		elif Input.is_action_pressed("Slow"):
+			sprite.rotation_degrees = -7.5
+		elif Input.is_action_just_released("Slow"):
+			sprite.rotation_degrees = 0
+		
+		#ifs for particles
+		if Input.is_action_just_pressed("Throw") && !is_on_floor():
+			spendToJump.emitting = true
+		elif Input.is_action_pressed("Slow") && is_on_floor():
+			skidding.emitting = true
+		elif Input.is_action_pressed("Speed") && is_on_floor():
+			run.emitting = true
+		elif Input.is_action_pressed("Jump") && is_on_floor():
+			jump.emitting = true
+		elif Input.is_action_pressed("Jump") && Input.is_action_pressed("Speed"):
+			jump.emitting = true
+		elif Input.is_action_pressed("Jump") &&  Input.is_action_pressed("Slow"):
+			jump.emitting = true
 
-			#ifs for sounds
-			if  Input.is_action_just_pressed("Jump") && is_on_floor():
-				jumping1.play()
-			elif Input.is_action_just_pressed("Jump") && !is_on_floor():
-				jumping2.play()
-			elif Input.is_action_just_pressed("Throw") && !is_on_floor():
-				coin.play()
-				jumping4.play()
-			elif !is_on_floor(): 
-				running.play()
-			
+		#ifs for sounds
+		if  Input.is_action_just_pressed("Jump") && is_on_floor():
+			jumping1.play()
+		elif Input.is_action_just_pressed("Jump") && !is_on_floor():
+			jumping2.play()
+		elif Input.is_action_just_pressed("Throw") && !is_on_floor():
+			coin.play()
+			jumping4.play()
+		elif !is_on_floor(): 
+			running.play()
+		
---
+++ scenes/characterSpawner.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+var characterLoader
+var _character
+
+func _ready():
+	if(Global.character=='Athlete'):
+		characterLoader= preload("res://scenes/Athlete.tscn")
+	elif (Global.character=='businessMan'):
+		characterLoader=preload("res://scenes/businessMan.tscn")
+	elif(Global.character=='FatMerchant'):
+		characterLoader=preload("res://scenes/FatMerchant.tscn")
+	
+	_character= characterLoader.instantiate()
+	_character.position = Vector2(212, 626)
+	add_child(_character)
+		
+func _process(_delta):
+	pass
+
+
+		
